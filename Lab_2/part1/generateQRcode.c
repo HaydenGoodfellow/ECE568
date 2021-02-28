697076c7c74502c88bf168b9612e1a5b45060c2e
Joseph Sawaya <josephsawaya938@gmail.com>
2021-02-28 00:11:23 -0500

    removing print statements

--- Lab_2/part1/generateQRcode.c
+++ Lab_2/part1/generateQRcode.c
@@ -24,21 +24,21 @@ main(int argc, char * argv[])
         unsigned char * hexSecret = malloc(10);
         int i = 0;
         for(i = 0; i < 20; i+=2){
-            sscanf(&secret_hex[i], "%02X", &hexSecret[i/2]);
+            sscanf(&secret_hex[i], "%2hhX", &hexSecret[i/2]);
         } 
  
         
         base32_encode(hexSecret, 10, base32EncodedSecret, 16);
         
-	printf("\nIssuer: %s\nAccount Name: %s\nSecret (Hex): %s\n\n",
-		issuer, accountName, secret_hex);
-        printf("%s\n", base32EncodedSecret);
+
 	// Create an otpauth:// URI and display a QR code that's compatible
 	// with Google Authenticator
         int bruh = 15 + strlen(accountName) + 8 + strlen(issuer) + 8 + 20;
         char * url = malloc(bruh);
         sprintf(url, "otpauth://totp/%s?issuer=%s&secret=%s&period=30", accountName, issuer, base32EncodedSecret);
 	displayQRcode(url);
-
+        free(base32EncodedSecret);
+        free(hexSecret);
+        free(url);
 	return (0);
 }

